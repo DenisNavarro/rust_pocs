591147c2bb906255c585b265df344f8a47de821d
Denis Navarro <denis.navarro@mailfence.com>
2022-12-17 17:48:04 +0100

    :hammer: :memo: :bulb: Simplify the Makefile and update doc and comments

--- structured_concurrency/src/main.rs
+++ structured_concurrency/src/main.rs
@@ -1,6 +1,6 @@
 // Adapted from:
 // https://github.com/rust-lang/book/blob/8d3584f55fa7f70ee699016be7e895d35d0e9b27/listings/ch20-web-server/no-listing-07-final-code/src/main.rs
-// The original code lacks error handling. I don't know yet if I will fix that too in this POC.
+// The original code lacks error handling. To limit the differences, I did not fix it.
 
 #![warn(clippy::nursery, clippy::pedantic)]
 #![allow(clippy::unused_io_amount)]
