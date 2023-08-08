15c38fd530c6b8c130697647545fa7b40b05566f
Denis Navarro <denis.navarro@mailfence.com>
2023-08-08 21:02:46 +0200

    :recycle: Use the inner function trick like in `std::fs::read`

--- bin_from_ninja/test_helper/src/lib.rs
+++ bin_from_ninja/test_helper/src/lib.rs
@@ -21,35 +21,48 @@ impl<T> Check for T
 where
     T: AsRef<Path>,
 {
+    // Remark: the below functions has an `inner` function, like the implementation of
+    // `std::fs::read`. This trick is explained in:
+    // The Rust Performance Book -> 20. Compile Times -> LLVM IR
+    // <https://github.com/nnethercote/perf-book/blob/3dc0a98387ac5cd93e3edbaa691e412662bd2b43/src/compile-times.md#llvm-ir>
+
     fn check_does_not_exist(&self) -> anyhow::Result<()> {
-        let path = self.as_ref();
-        path.symlink_metadata().is_err().then_some(()).with_context(|| format!("{path:?} exists"))
+        fn inner(path: &Path) -> anyhow::Result<()> {
+            ensure!(path.symlink_metadata().is_err(), "{path:?} exists");
+            Ok(())
+        }
+        inner(self.as_ref())
     }
 
     fn check_is_dir(&self) -> anyhow::Result<()> {
-        let path = self.as_ref();
-        let metadata = symlink_metadata(path)?;
-        ensure!(metadata.is_dir(), "{path:?} exists but is not a directory");
-        Ok(())
+        fn inner(path: &Path) -> anyhow::Result<()> {
+            let metadata = symlink_metadata(path)?;
+            ensure!(metadata.is_dir(), "{path:?} exists but is not a directory");
+            Ok(())
+        }
+        inner(self.as_ref())
     }
 
     fn check_is_file_with_content(&self, expected: impl AsRef<str>) -> anyhow::Result<()> {
-        let path = self.as_ref();
-        let expected = expected.as_ref();
-        let metadata = symlink_metadata(path)?;
-        ensure!(metadata.is_file(), "{path:?} exists but is not a file");
-        let cont = fs::read(path).with_context(|| format!("failed to read {path:?}"))?;
-        let cont = String::from_utf8(cont).with_context(|| format!("non-UTF8 data in {path:?}"))?;
-        ensure!(cont == expected, "the content of {path:?} is {cont:?}, not {expected:?}");
-        Ok(())
+        fn inner(path: &Path, expected: &str) -> anyhow::Result<()> {
+            let metadata = symlink_metadata(path)?;
+            ensure!(metadata.is_file(), "{path:?} exists but is not a file");
+            let cont = fs::read(path).with_context(|| format!("failed to read {path:?}"))?;
+            let cont =
+                String::from_utf8(cont).with_context(|| format!("non-UTF8 data in {path:?}"))?;
+            ensure!(cont == expected, "the content of {path:?} is {cont:?}, not {expected:?}");
+            Ok(())
+        }
+        inner(self.as_ref(), expected.as_ref())
     }
 
     fn check_is_symlink_to(&self, expected: impl AsRef<Path>) -> anyhow::Result<()> {
-        let path = self.as_ref();
-        let expected = expected.as_ref();
-        let target = path.read_link().with_context(|| format!("{path:?} is not a symlink"))?;
-        ensure!(target == expected, "{path:?} is a symlink to {target:?}, not {expected:?}");
-        Ok(())
+        fn inner(path: &Path, expected: &Path) -> anyhow::Result<()> {
+            let target = path.read_link().with_context(|| format!("{path:?} is not a symlink"))?;
+            ensure!(target == expected, "{path:?} is a symlink to {target:?}, not {expected:?}");
+            Ok(())
+        }
+        inner(self.as_ref(), expected.as_ref())
     }
 }
 
