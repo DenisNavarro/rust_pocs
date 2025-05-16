7e37988ec292a6a751a4aa084848d702362181d7
Denis Navarro <denis.navarro@mailfence.com>
2025-05-16 18:07:19 +0200

    :arrow_up: Upgrade dependencies, including Rust from 1.86.0 to 1.87.0

--- pre-commit.sh
+++ pre-commit.sh
@@ -10,15 +10,15 @@ if ! git diff --cached --quiet -- bin_from_ninja; then (
 if ! git diff --cached --quiet -- coroutine; then (
     cd coroutine
     cargo +1.85.0 test --locked --workspace
-    cargo +1.86.0 fmt --all --check
-    cargo +1.86.0 clippy --all-features --all-targets --locked --workspace -- -D warnings
-    cargo +1.86.0 test --locked --workspace
+    cargo +1.87.0 fmt --all --check
+    cargo +1.87.0 clippy --all-features --all-targets --locked --workspace -- -D warnings
+    cargo +1.87.0 test --locked --workspace
 ) fi
 
 if ! git diff --cached --quiet -- structured_concurrency; then (
     cd structured_concurrency
     cargo +1.85.0 check --locked
-    cargo +1.86.0 fmt --check
-    cargo +1.86.0 clippy --all-features --all-targets --locked -- -D warnings
-    cargo +1.86.0 check --locked
+    cargo +1.87.0 fmt --check
+    cargo +1.87.0 clippy --all-features --all-targets --locked -- -D warnings
+    cargo +1.87.0 check --locked
 ) fi
