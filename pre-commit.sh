3eb50a8847ae1c80625e115a313afc0dccbad978
Denis Navarro <denis.navarro@mailfence.com>
2025-11-16 04:54:31 +0100

    :arrow_up: Upgrade dependencies, including Rust from 1.90.0 to 1.91.1

--- pre-commit.sh
+++ pre-commit.sh
@@ -10,15 +10,15 @@ if ! git diff --cached --quiet -- bin_from_ninja; then (
 if ! git diff --cached --quiet -- coroutine; then (
     cd coroutine
     cargo +1.85.0 test --locked --workspace
-    cargo +1.90.0 fmt --all --check
-    cargo +1.90.0 clippy --all-features --all-targets --locked --workspace -- -D warnings
-    cargo +1.90.0 test --locked --workspace
+    cargo +1.91.1 fmt --all --check
+    cargo +1.91.1 clippy --all-features --all-targets --locked --workspace -- -D warnings
+    cargo +1.91.1 test --locked --workspace
 ) fi
 
 if ! git diff --cached --quiet -- structured_concurrency; then (
     cd structured_concurrency
     cargo +1.85.0 check --locked
-    cargo +1.90.0 fmt --check
-    cargo +1.90.0 clippy --all-features --all-targets --locked -- -D warnings
-    cargo +1.90.0 check --locked
+    cargo +1.91.1 fmt --check
+    cargo +1.91.1 clippy --all-features --all-targets --locked -- -D warnings
+    cargo +1.91.1 check --locked
 ) fi
